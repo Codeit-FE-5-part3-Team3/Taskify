37341e5c7444139b7f8f9d43fc29793e63a22849
bk-git-hub <bkprivate99@gmail.com>
2024-06-05 15:44:03 +0900

    refactor: Replace data fetching with serverSideFetcher component

--- src/components/side-bar/SideBar.tsx
+++ src/components/side-bar/SideBar.tsx
@@ -1,4 +1,5 @@
 import { authOptions } from "@/lib/auth";
+import { serverSideFetcher } from "@/lib/utils";
 import { getServerSession } from "next-auth";
 import Image from "next/image";
 import Link from "next/link";
@@ -17,26 +18,16 @@ type dashboard = {
   userId: number;
 };
 
-async function getDashboards() {
-  const session = await getServerSession(authOptions);
-  if (!session) {
-    return null;
-  }
-  const accessToken = session.accessToken;
-  const response = await fetch(
+async function getDashBoards() {
+  const response = await serverSideFetcher(
     "https://sp-taskify-api.vercel.app/5-3/dashboards?navigationMethod=infiniteScroll&page=1&size=9999",
-    {
-      headers: {
-        Authorization: `Bearer ${accessToken}`,
-      },
-    },
   );
-  const data = await response.json();
+  const data = await response?.json();
   return data.dashboards;
 }
 
 export default async function SideBar({ selectedId }: Props) {
-  const dashboards = await getDashboards();
+  const dashboards = await getDashBoards();
   if (!dashboards) return;
   return (
     <div className="flex flex-col w-[300px] h-[100vh] px-[12px] border border-[#d9d9d9]">
