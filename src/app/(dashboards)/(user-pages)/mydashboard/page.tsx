1f277272cb1109b92399a03bc53d81efa95fa69f
bk-git-hub <bkprivate99@gmail.com>
2024-06-10 11:20:43 +0900

    feat: Add dashboard create button in my dashboard page

--- src/app/(dashboards)/(user-pages)/mydashboard/page.tsx
+++ src/app/(dashboards)/(user-pages)/mydashboard/page.tsx
@@ -1,3 +1,13 @@
+import DashboardCreateButton from "./components/DashboardCreateButton";
+
 export default function MyDashboardPage() {
-  return <main>나의 대시보드</main>;
+  return (
+    <main className="p-10">
+      <ul className="grid grid-cols-3 gap-3">
+        <li>
+          <DashboardCreateButton />
+        </li>
+      </ul>
+    </main>
+  );
 }
