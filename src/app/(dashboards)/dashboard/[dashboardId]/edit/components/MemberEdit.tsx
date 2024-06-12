8a196de1757346e2748ab6909ab5f27869ae4d25
bk-git-hub <bkprivate99@gmail.com>
2024-06-12 14:38:10 +0900

    feat: Add paginated member list in edit page

--- src/app/(dashboards)/dashboard/[dashboardId]/edit/components/MemberEdit.tsx
+++ src/app/(dashboards)/dashboard/[dashboardId]/edit/components/MemberEdit.tsx
@@ -1,5 +1,7 @@
 import PaginationButtonBar from "@/components/pagination-button-bar/PaginationButtonBar";
 import { getPageMembers } from "@/util/api/getPageMember";
+import { Avatar } from "@/components/ui/avatar";
+import CustomAvatar from "@/components/custom-avatar/CustomAvatar";
 
 interface Props {
   memberPage: number;
@@ -13,14 +15,15 @@ export default async function MemberEdit({
   dashboardId,
 }: Props) {
   const { members, totalCount } = await getPageMembers(memberPage, dashboardId);
-  const maxPage = Math.max(1, totalCount / 4);
+  const maxPage = Math.max(1, Math.ceil(totalCount / 4));
 
   const isLastMemberPage = memberPage === maxPage;
   const isFirstMemberPage = memberPage === 1;
   const currentUrl = `/dashboard/${dashboardId}/edit`;
+
   return (
-    <div className=" space-y-6 rounded-lg bg-blue-100 p-7 flex flex-col h-[404px]">
-      <div className="w-full flex justify-between">
+    <div className="rounded-lg bg-blue-100 p-7 flex flex-col h-[460px]">
+      <div className="w-full flex justify-between items-center">
         <span className="text-2xl font-bold">구성원</span>
 
         <div className="flex items-center gap-4">
@@ -41,6 +44,33 @@ export default async function MemberEdit({
           </div>
         </div>
       </div>
+      <span className="text-gray-400 mt-7">이름</span>
+      <ul>
+        {members.map((member: any, index: number) => (
+          <li
+            key={index}
+            className={`py-4 flex justify-between ${
+              index !== members.length - 1 ? "border-b border-gray-300" : ""
+            }`}
+          >
+            <div className="flex gap-3 items-center">
+              <Avatar>
+                <CustomAvatar
+                  imgUrl={member.profileImageUrl}
+                  nickname={member.nickname}
+                  userId={member.userId}
+                  size={38}
+                />
+              </Avatar>
+              <span>{member.nickname}</span>
+            </div>
+
+            <button className="text-violet-100 px-7 py-2 border border-gray-300 rounded">
+              삭제
+            </button>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
