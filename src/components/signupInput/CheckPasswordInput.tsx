a4197bf738fb29636bc747025cc07c948e5e3fb7
ehdus3255 <rlaehdus3255@naver.com>
2024-06-12 18:29:14 +0900

    회원가입 ui 코드 작성

--- src/components/signupInput/CheckPasswordInput.tsx
+++ src/components/signupInput/CheckPasswordInput.tsx
@@ -6,16 +6,16 @@ import {
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 
-export function PasswordInput() {
+export function CheckPasswordInput() {
   return (
     <FormField
       name="name"
       render={({ field }) => {
         return (
           <FormItem>
-            <FormLabel className="text-lg">비밀번호</FormLabel>
+            <FormLabel className="text-lg">비밀번호 확인</FormLabel>
             <FormControl>
-              <Input {...field} placeholder="비밀번호를 입력해 주세요" className="text-[16px] h-12 font-normal focus:border-violet-100 focus:border-[1px] focus:border-solid" />
+              <Input {...field} placeholder="비밀번호를 한번 더 입력해 주세요" className="text-[16px] h-12 font-normal focus:border-violet-100 focus:border-[1px] focus:border-solid" />
             </FormControl>
           </FormItem>
         );
--- src/components/signupInput/PasswordInput.tsx
+++ src/components/signupInput/PasswordInput.tsx
@@ -15,7 +15,7 @@ export function PasswordInput() {
           <FormItem>
             <FormLabel className="text-lg">비밀번호</FormLabel>
             <FormControl>
-              <Input {...field} placeholder="비밀번호를 입력해 주세요" className="text-[16px] h-12 font-normal focus:border-violet-100 focus:border-[1px] focus:border-solid" />
+              <Input {...field} placeholder="8자 이상 입력해 주세요" className="text-[16px] h-12 font-normal focus:border-violet-100 focus:border-[1px] focus:border-solid" />
             </FormControl>
           </FormItem>
         );
