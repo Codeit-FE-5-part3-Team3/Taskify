7928b260642403e41b4d8c74cbdff014c5cf5631
bk-git-hub <bkprivate99@gmail.com>
2024-06-13 22:41:51 +0900

    refactor: Refactor POST and PUT endpoints to use NextResponse.json() for improved response handling

--- src/app/api/dashboards/invitations/route.ts
+++ src/app/api/dashboards/invitations/route.ts
@@ -1,13 +1,12 @@
 import { getServerSession } from "next-auth";
 import { authOptions } from "@/lib/auth";
+import { NextResponse } from "next/server";
 
-export async function POST(req: any) {
+export async function POST(req: Request) {
   const session = await getServerSession(authOptions);
 
   if (!session) {
-    return new Response(JSON.stringify({ message: "Unauthorized" }), {
-      status: 401,
-    });
+    return NextResponse.json({ message: "Unauthorized" }, { status: 401 });
   }
 
   const body = await req.json();
@@ -27,25 +26,21 @@ export async function POST(req: any) {
 
   const data = await backendResponse.json();
 
-  return new Response(JSON.stringify(data), {
-    status: backendResponse.status,
-  });
+  return NextResponse.json(data, { status: backendResponse.status });
 }
 
-export async function PUT(req: any) {
+export async function PUT(req: Request) {
   const session = await getServerSession(authOptions);
 
   if (!session) {
-    return new Response(JSON.stringify({ message: "Unauthorized" }), {
-      status: 401,
-    });
+    return NextResponse.json({ message: "Unauthorized" }, { status: 401 });
   }
 
   const body = await req.json();
   console.log(body);
   const invitationId = Number(body.invitationId);
-  const invitedAccepted = body.accepted;
-  console.log(invitedAccepted);
+  const inviteAccepted = body.accepted;
+  console.log(inviteAccepted);
 
   const backendResponse = await fetch(
     `https://sp-taskify-api.vercel.app/5-3/invitations/${invitationId}`,
@@ -55,13 +50,11 @@ export async function PUT(req: any) {
         Authorization: `Bearer ${session.accessToken}`,
         "Content-Type": "application/json",
       },
-      body: JSON.stringify({ inviteAccepted: invitedAccepted }),
+      body: JSON.stringify({ inviteAccepted }),
     },
   );
 
   const data = await backendResponse.json();
 
-  return new Response(JSON.stringify(data), {
-    status: backendResponse.status,
-  });
+  return NextResponse.json(data, { status: backendResponse.status });
 }
