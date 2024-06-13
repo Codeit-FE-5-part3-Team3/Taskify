b1a2b12d31e08822ea1ee513eea5f046843ee4d5
bk-git-hub <bkprivate99@gmail.com>
2024-06-13 15:16:28 +0900

    feat: Add dashboard delete button

---
+++ src/app/api/dashboards/[dashboardId]/route.ts
@@ -0,0 +1,39 @@
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/lib/auth";
+
+export async function DELETE(req: any) {
+  const session = await getServerSession(authOptions);
+
+  if (!session) {
+    return new Response(JSON.stringify({ message: "Unauthorized" }), {
+      status: 401,
+    });
+  }
+
+  // req.nextUrl.pathname에서 memberId 추출
+  const pathname = req.nextUrl.pathname;
+  const dashboardId = pathname.split("/").pop();
+  console.log(dashboardId);
+  if (!dashboardId) {
+    return new Response(JSON.stringify({ message: "memberId is required" }), {
+      status: 400,
+    });
+  }
+
+  const backendResponse = await fetch(
+    `https://sp-taskify-api.vercel.app/5-3/dashboards/${dashboardId}`,
+    {
+      method: "DELETE",
+      headers: {
+        Authorization: `Bearer ${session.accessToken}`,
+        "Content-Type": "application/json",
+      },
+    },
+  );
+
+  const data = await backendResponse.json();
+
+  return new Response(JSON.stringify(data), {
+    status: backendResponse.status,
+  });
+}
