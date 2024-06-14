4632576fb3627db8af860c6e792a1a003ff90479
bk-git-hub <bkprivate99@gmail.com>
2024-06-14 15:20:47 +0900

    질척거리는 타입

--- src/app/(dashboards)/dashboard/[dashboardId]/components/delete-column-modal/DeleteColumnModal.tsx
+++ src/app/(dashboards)/dashboard/[dashboardId]/components/delete-column-modal/DeleteColumnModal.tsx
@@ -22,7 +22,15 @@ export default function DeleteColumnModal({
     <>
       <AlertDialogHeader>
         <AlertDialogTitle>
-          <b>{title}</b> 칼럼을 삭제하시겠습니까??
+          진짜로 다시한번만 생각해보세요. 여태까지 한게 있지않습니까? 아깝지
+          않습니까? 이거 지우면 다시 복원도 안돼요. 카드도 전부 삭제되고.. 이거
+          삭제 버튼 누르면 다시 못돌아와요. 진짜로 확실해요? 카드만
+          삭제할려했던거 아니고? 정말 이렇게까지 해야합니까? 다시한번
+          묻겠습니다.
+          <br />
+          <br />
+          진짜로 <b>{title}</b>칼럼을
+          <span className="text-red-500"> 삭제해야만 되겠습니까??</span>
         </AlertDialogTitle>
       </AlertDialogHeader>
       <AlertDialogFooter>
