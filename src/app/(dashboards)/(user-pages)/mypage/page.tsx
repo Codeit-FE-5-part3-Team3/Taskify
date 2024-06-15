b15e468f6cf354a233e1f29fffa66d9092ea196a
Erin <skaurd12323@gmail.com>
2024-06-16 05:14:27 +0900

    feat: Add Link and Toaster

--- src/app/(dashboards)/(user-pages)/mypage/page.tsx
+++ src/app/(dashboards)/(user-pages)/mypage/page.tsx
@@ -1,18 +1,25 @@
 import { Button } from "@/components/ui/button";
 import { ChevronLeftIcon } from "lucide-react";
-import { PasswordForm } from "./_components/PasswordForm";
 import { ProfileFormData } from "./_components/ProfileFormData";
+import Link from "next/link";
+import { PasswordForm } from "./_components/PasswordForm";
+import { Toaster } from "@/components/ui/toaster";
 
-export default function MyDashboardPage() {
+export default function Mypage() {
   return (
     <main className="h-dvh w-full bg-[#FAFAFA] p-5">
-      <Button className="gap-1 px-0 pr-1 text-base" variant="ghost">
-        <ChevronLeftIcon className="size-6 stroke-[1.5]" />
-        돌아가기
-      </Button>
-      <div className="mt-6 flex flex-col gap-3">
-        <ProfileFormData />
-        <PasswordForm />
+      <div>
+        <Button className="p-0 text-base" variant="ghost">
+          <Link href="/mydashboard" className="flex gap-1">
+            <ChevronLeftIcon className="size-6 stroke-[1.5]" />
+            돌아가기
+          </Link>
+        </Button>
+        <div className="mt-6 flex flex-col gap-3">
+          <ProfileFormData />
+          <PasswordForm />
+        </div>
+        <Toaster />
       </div>
     </main>
   );
