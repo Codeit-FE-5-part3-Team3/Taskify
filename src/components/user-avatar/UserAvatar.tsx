d6bcb374d3f7a4317847beb73ef7043963a0ad85
bk-git-hub <bkprivate99@gmail.com>
2024-06-15 21:01:50 +0900

    feat: Add features to AvatarDropDown

--- src/components/user-avatar/UserAvatar.tsx
+++ src/components/user-avatar/UserAvatar.tsx
@@ -10,6 +10,8 @@ import { serverSideFetcher } from "@/lib/utils";
 import { cn } from "@/lib/utils";
 import CustomAvatar from "../custom-avatar/CustomAvatar";
 import { Avatar } from "@/components/ui/avatar";
+import { signOut } from "next-auth/react";
+import UserAvatarUI from "./UserAvatarUI";
 
 async function getUserInfo() {
   const response = await serverSideFetcher(
@@ -22,36 +24,5 @@ async function getUserInfo() {
 export default async function UserAvatar({}) {
   const userData = await getUserInfo();
 
-  return (
-    <DropdownMenu>
-      <DropdownMenuTrigger asChild className="outline-none">
-        <button className="flex gap-3 pl-6 items-center border-l border-[#d9d9d9]">
-          <Avatar>
-            <CustomAvatar
-              imgUrl={userData.profileImageUrl}
-              nickname={userData.nickname}
-              userId={userData.id}
-              size={38}
-            />
-          </Avatar>
-          <span className="font-medium">{userData.nickname}</span>
-        </button>
-      </DropdownMenuTrigger>
-
-      <DropdownMenuContent>
-        <DropdownMenuLabel>계정</DropdownMenuLabel>
-        <DropdownMenuSeparator />
-        <DropdownMenuItem
-          className={cn("focus:bg-[#f1effd] focus:text-[#5534da]")}
-        >
-          마이페이지
-        </DropdownMenuItem>
-        <DropdownMenuItem
-          className={cn(" focus:bg-[#f1effd] focus:text-[#5534da]")}
-        >
-          로그아웃
-        </DropdownMenuItem>
-      </DropdownMenuContent>
-    </DropdownMenu>
-  );
+  return <UserAvatarUI userData={userData} />;
 }
