5bb77e8cdae617e716b9ae7774428d177d03f91e
bk <77979028+bk-git-hub@users.noreply.github.com>
2025-02-25 16:41:11 +0900

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,20 +3,20 @@ const MainPage = () => {
     <>
       <main className='flex flex-center flex-col gap-24 bg-gray-900'>
         <p>
-      ----------------이미본것들------------<br>
+      ----------------이미본것들------------<br/>
 keeping-components-pure<br>
-<br>
-----------------봐야할것들-------------<br>
-----/learn/<br>
-describing-the-ui<br>
-responding-to-events<br>
-understanding-your-ui-as-a-tree<br>
-render-and-commit<br>
-synchronizing-with-effects<br>
-<br>
----/reference/<br>
-react/memo<br>
-react/useEffect<br>
+<br/>
+----------------봐야할것들-------------<br/>
+----/learn/<br/>
+describing-the-ui<br/>
+responding-to-events<br/>
+understanding-your-ui-as-a-tree<br/>
+render-and-commit<br/>
+synchronizing-with-effects<br/>
+<br/>
+---/reference/<br/>
+react/memo<br/>
+react/useEffect<br/>
   </p>
       </main>
     </>
